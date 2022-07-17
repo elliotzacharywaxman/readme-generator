c61e001c847649e2db472fe0a2002295db308152
elliotzacharywaxman <elliot@climatedao.xyz>
2022-07-17 13:36:48 -0400

    finalizing readme

--- index.js
+++ index.js
@@ -7,34 +7,49 @@ inquirer
   .prompt([
     /* Pass your questions in here */
     {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?"
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?"
     },
     {
-        type: "input",
-        name: "description",
-        message: "What is the description of your project?"
+      type: "input",
+      name: "description",
+      message: "What is the description of your project?"
     },
     {
-        type: "input",
-        name: "usage",
-        message: "What is the usage of your project?"
+      type: "input",
+      name: "usage",
+      message: "What is the usage of your project?(What are some examples of how it's used?)"
     },
     {
-        type: "input",
-        name: "installation",
-        message: "What is the installation of your project?"
+      type: "input",
+      name: "installation",
+      message: "What is the installation of your project? (steps you had to take to install it)"
     },
     {
-        type: "list",
-        choices: ["apache 2.0", "MIT", "GPL", "BSD 3.0"],
-        name: "license",
-        message: "What is the license of your project?"
-    }
+      type: "list",
+      choices: ["apache 2.0", "MIT", "GPL", "BSD 3.0"],
+      name: "license",
+      message: "What is the license of your project?"
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Who is welcome to contribute to this project?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your github?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?"
+    },
   ])
   .then((answers) => {
-    fs.writeFileSync(path.join(__dirname,"/dist/","README.md"),generateMarkdown(answers))
+    fs.writeFileSync(path.join(__dirname, "/dist/", "README.md"), generateMarkdown(answers))
     console.log(answers)
   })
   .catch((error) => {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === "MIT"){
+  if (license === "MIT") {
     return `[![MIT](https://img.shields.io/badge/License-MIT-blue)](https://opensource.org/licenses/MIT)`
   } else if
-    (license === "apache 2.0"){
+    (license === "apache 2.0") {
     return `[![Apache](https://img.shields.io/badge/License-apache%202.0-yellowgreen)](https://opensource.org/licenses/apache%202.0)`
   } else if
-  (license === "GPL"){
-  return `[![Apache](https://img.shields.io/badge/License-GPL-green)](https://opensource.org/licenses/apache%202.0)`
+    (license === "GPL") {
+    return `[![GPL](https://img.shields.io/badge/License-GPL-green)](https://img.shields.io/badge/License-GPL-brightgreen)`
   } else if
-  (license === "APL"){
-  return `[![Apache](https://img.shields.io/badge/License-GPL-green)](https://opensource.org/licenses/apache%202.0)`
+    (license === "APL") {
+    return `[![APL](https://img.shields.io/badge/License-APL-blue)](https://img.shields.io/badge/License-APL-blue)`
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-${renderLicenseBadge(data.license)}
+  console.log(data)
+  return `# ${data.title} 
+  ## Licenses
+  ${renderLicenseBadge(data.license)}
+  ## Description 
+  ${data.description}
+  ## Installation 
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ## Questions
+  Feel free to reach out to ${data.email} or check out this git hub repo https://github.com/${data.github}
 `;
 }
 
